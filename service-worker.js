3b3d5d3097986085c8ead20b90e1922d0b6e65bb
xXMajinKaiohXx <lennart.fredriksson@bahnhof.se>
2025-04-26 14:42:50 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('brutal-logg').then(function(cache) {
+      return cache.addAll(['index.html']);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
